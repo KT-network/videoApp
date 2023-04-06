7c83d86be44616b0883595b75337be18fe68c3d0
kunshao <kunshao@kun.cn>
2023-04-06 20:13:17 +0800

    新增了倍速播放和选集ui及功能

--- app/src/main/java/com/kt/coffee/cat/ktPlay/PlayerActivity.java
+++ app/src/main/java/com/kt/coffee/cat/ktPlay/PlayerActivity.java
@@ -1,27 +1,22 @@
 package com.kt.coffee.cat.ktPlay;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.kt.coffee.cat.R;
-import com.kt.coffee.cat.ktPlay.ui.KsIncompletionViewNew;
+import com.kt.coffee.cat.ktPlay.ui.AnthologyControl.KsAnthologyView;
+import com.kt.coffee.cat.ktPlay.ui.KsIncompletionView;
 import com.kt.coffee.cat.ktPlay.ui.KsLandscapeView;
-import com.kt.coffee.cat.ktPlay.ui.KsLandscapeViewNew;
 import com.kt.coffee.cat.ktPlay.ui.KsStandardVideoController;
+import com.kt.coffee.cat.ktPlay.ui.PlayerSpeedControl.KsPlayerSpeedView;
 import com.kt.coffee.cat.ktPlay.ui.component.KsErrorView;
 import com.kt.coffee.cat.ktPlay.ui.component.KsGestureView;
-import com.kt.coffee.cat.ktPlay.ui.component.KsIncompletionView;
-import com.kt.coffee.cat.ktPlay.ui.component.KsPlayerSpeedView;
-import com.kt.coffee.cat.ktPlay.ui.component.KsPrepareView;
-import com.kt.coffee.cat.ktPlay.ui.component.KsTitleMoreView;
-import com.kt.coffee.cat.ktPlay.ui.component.KsTitleView;
-import com.kt.coffee.cat.ktPlay.ui.component.KsVodControlView;
+import com.kt.coffee.cat.mInterface.ClickListener;
 import com.kt.coffee.cat.utils.PlayerVideoEntity;
 import com.kt.coffee.cat.utils.Tool;
 
@@ -36,7 +31,13 @@ public class PlayerActivity extends AppCompatActivity {
 
     VideoView mVideoView;
 
-    KsIncompletionViewNew ksIncompletionViewNew;
+    KsIncompletionView ksIncompletionView;
+
+    KsLandscapeView ksLandscapeView;
+
+    KsAnthologyView ksAnthologyView;
+    KsPlayerSpeedView ksPlayerSpeedView;
+    KsStandardVideoController controller;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -60,21 +61,51 @@ public class PlayerActivity extends AppCompatActivity {
         initView();
         initData();
 
-
-
     }
 
     private void initData() {
 
         try {
             String JSON = Tool.inputStreamToString(getResources().getAssets().open("Video.json"));
-            PlayerVideoEntity playerVideoEntity = new Gson().fromJson(JSON,PlayerVideoEntity.class);
+            PlayerVideoEntity playerVideoEntity = new Gson().fromJson(JSON, PlayerVideoEntity.class);
+
+            Log.i(TAG, "initData: " + playerVideoEntity.getVideoUrlArrays().get(0).getName());
+            ksAnthologyView.setListData(playerVideoEntity.getVideoUrlArrays());
+            mVideoView.setUrl(playerVideoEntity.getVideoUrlArrays().get(0).getVideoUrl());
 
 //            ksTitleMoreView.setTitle(playerVideoEntity.getVideoName());
 //            Glide.with(this).load(playerVideoEntity.getVideoThumb()).into(thumb);
-            mVideoView.setUrl(playerVideoEntity.getVideoUrl());
+//            mVideoView.setUrl(playerVideoEntity.getVideoUrl());
 
             mVideoView.start();
+
+            ksAnthologyView.setListItemClick(new ClickListener.OnClickListener() {
+                @Override
+                public void onClick(int i) {
+                    mVideoView.release();
+                    mVideoView.setUrl(playerVideoEntity.getVideoUrlArrays().get(i).getVideoUrl());
+                    mVideoView.setVideoController(controller);
+                    mVideoView.start();
+
+
+                    Tool.KToast(getApplicationContext(), String.valueOf(i));
+                }
+            });
+
+            ksPlayerSpeedView.setOnClickListener(new ClickListener.OnClickListener() {
+                @Override
+                public void onClick(int i) {
+
+                    if (i == 1) ksLandscapeView.setPlaySpeedText(R.string.play_speed_0_5);
+                    else if (i == 2) ksLandscapeView.setPlaySpeedText(R.string.play_speed_0_75);
+                    else if (i == 3) ksLandscapeView.setPlaySpeedText(R.string.play_speed_1_0);
+                    else if (i == 4) ksLandscapeView.setPlaySpeedText(R.string.play_speed_1_25);
+                    else if (i == 5) ksLandscapeView.setPlaySpeedText(R.string.play_speed_1_5);
+                    else if (i == 2) ksLandscapeView.setPlaySpeedText(R.string.play_speed_2_0);
+
+
+                }
+            });
 
 
         } catch (IOException e) {
@@ -84,16 +115,23 @@ public class PlayerActivity extends AppCompatActivity {
     }
 
 
-    private void initView(){
+    private void initView() {
         mVideoView = findViewById(R.id.player);
 
-        KsStandardVideoController controller = new KsStandardVideoController(this);
+
+        controller = new KsStandardVideoController(this);
+
+        ksLandscapeView = new KsLandscapeView(this);
+        ksIncompletionView = new KsIncompletionView(this);
+
+        ksAnthologyView = new KsAnthologyView(this);
+        ksPlayerSpeedView = new KsPlayerSpeedView(this);
+
         controller.setEnableOrientation(false);  // 设置是否自动旋转屏幕
 
 
-        ksIncompletionViewNew = new KsIncompletionViewNew(this);
-        controller.addControlComponent(ksIncompletionViewNew);
-        ksIncompletionViewNew.setBack(new View.OnClickListener() {
+        controller.addControlComponent(ksIncompletionView);
+        ksIncompletionView.setBack(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
@@ -101,35 +139,43 @@ public class PlayerActivity extends AppCompatActivity {
         });
 
 
-//        prepareView = new KsPrepareView(this);
-//        prepareView.setClickStart();
-//        thumb = prepareView.findViewById(R.id.thumb);
-////        Glide.with(this).load(THUMB).into(thumb);
-//        controller.addControlComponent(prepareView);
-
         controller.addControlComponent(new CompleteView(this));
         controller.addControlComponent(new KsErrorView(this));//错误界面
 
         controller.addControlComponent(new KsGestureView(this));
 
 
+        controller.addControlComponent(ksLandscapeView);
+        controller.addControlComponent(ksAnthologyView);
+        controller.addControlComponent(ksPlayerSpeedView);
+        ksLandscapeView.getPlayForwardView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ksLandscapeView.hide();
+                ksAnthologyView.show();
+            }
+        });
 
-        controller.addControlComponent(new KsLandscapeViewNew(this));
+
+        ksLandscapeView.getPlaySpeedView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ksLandscapeView.hide();
+                ksPlayerSpeedView.show();
+            }
+        });
+
 
         mVideoView.setVideoController(controller);
 
     }
 
 
-
-
-
-
     @Override
     protected void onResume() {
         super.onResume();
 
-        if (mVideoView != null){
+        if (mVideoView != null) {
             mVideoView.resume();
         }
     }
---
+++ app/src/main/java/com/kt/coffee/cat/ktPlay/ui/AnthologyControl/KsAnthologyView.java
@@ -0,0 +1,106 @@
+package com.kt.coffee.cat.ktPlay.ui.AnthologyControl;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kt.coffee.cat.R;
+import com.kt.coffee.cat.ktPlay.ui.KsControlAbstract;
+import com.kt.coffee.cat.mInterface.ClickListener;
+import com.kt.coffee.cat.utils.PlayerVideoEntity;
+
+import java.util.List;
+
+import xyz.doikki.videoplayer.controller.ControlWrapper;
+import xyz.doikki.videoplayer.controller.IControlComponent;
+
+public class KsAnthologyView extends KsControlAbstract implements IControlComponent {
+
+
+    private ControlWrapper mControlWrapper;
+    private KsAnthologyAdapter ksAnthologyAdapter;
+
+    public KsAnthologyView(Context context) {
+        super(context);
+        super.width = 250.0F;
+        this.initAnimation();
+        initList();
+    }
+
+    private void initList(){
+        RecyclerView recyclerView = super.view.findViewById(R.id.play_select_RecyclerView);
+        ksAnthologyAdapter = new KsAnthologyAdapter(super.context);
+        recyclerView.setLayoutManager(new LinearLayoutManager(super.context));
+        recyclerView.setAdapter(ksAnthologyAdapter);
+    }
+
+    public void setListData(List<PlayerVideoEntity.VideoUrlArray> data){
+        ksAnthologyAdapter.setData(data);
+    }
+
+    public void setListAddData(){
+
+    }
+
+    public void setListItemClick(ClickListener.OnClickListener onClickListener){
+        ksAnthologyAdapter.setOnClick(onClickListener);
+    }
+
+    public void setItemState(int position, LinearLayout linearLayout){
+        ksAnthologyAdapter.setItemState(position,linearLayout);
+    }
+
+    @Override
+    public int getBGID() {
+        return R.id.landscape_select;
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.ktplayer_layout_anthology_view;
+    }
+
+    @Override
+    public void attach(@NonNull ControlWrapper controlWrapper) {
+        this.mControlWrapper = controlWrapper;
+    }
+
+    @Nullable
+    @Override
+    public View getView() {
+        return super.view;
+    }
+
+    @Override
+    public void onVisibilityChanged(boolean isVisible, Animation anim) {
+
+    }
+
+    @Override
+    public void onPlayStateChanged(int playState) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(int playerState) {
+        if (playerState == 10){
+            super.view.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void setProgress(int duration, int position) {
+
+    }
+
+    @Override
+    public void onLockStateChanged(boolean isLocked) {
+
+    }
+}
